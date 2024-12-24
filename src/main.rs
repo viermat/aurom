5928654af145aae6b291fc934b67fc55c8148b12
alex <jipes.alex@gmail.com>
2024-12-24 22:11:03 +0200

    changed messages a bit, added incognito and stealth (no longer enabled by default since it is worst than not having it) flag, small code refactor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::ffi::OsStr;
 use clap::{
     crate_description, crate_name, crate_version, Arg, ArgAction, ArgGroup, Command, ValueHint,
 };
-use headless_chrome::{protocol::cdp::Network::Cookie, Browser};
+
+use headless_chrome::Browser;
 
 static USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
 
@@ -22,7 +23,7 @@ fn main() {
                 .short('c')
                 .value_hint(ValueHint::Url)
                 .value_name("ws_url")
-                .help("Connect to an existing browser instance via WebSocket")
+                .help("Connect to an existing browser instance via WebSocket.")
                 .action(ArgAction::Set)
                 .conflicts_with("new"),
         )
@@ -30,7 +31,7 @@ fn main() {
             Arg::new("new")
                 .long("new")
                 .short('n')
-                .help("Launch a new browser instance")
+                .help("Launch a new browser instance.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
@@ -39,14 +40,14 @@ fn main() {
                 .short('u')
                 .value_hint(ValueHint::AnyPath)
                 .value_name("target_url")
-                .help("Set an URL for the target tab")
+                .help("Set an URL for the target tab.")
                 .action(ArgAction::Set),
         )
         .arg(
             Arg::new("headful")
                 .long("headful")
                 .short('H')
-                .help("Run browser in headful mode (requires --new)")
+                .help("Run browser in headful mode (requires --new).")
                 .requires("new")
                 .action(ArgAction::SetFalse),
         )
@@ -54,7 +55,7 @@ fn main() {
             Arg::new("clean")
                 .long("clean")
                 .short('C')
-                .help("Start the tab with a clean localStorage and cookies")
+                .help("Start the tab with a clean localStorage and cookies.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
@@ -63,7 +64,7 @@ fn main() {
                 .short('p')
                 .value_hint(ValueHint::AnyPath)
                 .value_name("file")
-                .help("Specify a JavaScript payload file to inject into the target tab")
+                .help("Specify a JavaScript payload file to inject into the target tab.")
                 .action(ArgAction::Set),
         )
         .arg(
@@ -72,36 +73,51 @@ fn main() {
                 .short('o')
                 .value_hint(ValueHint::AnyPath)
                 .value_name("file")
-                .help("Output cookies and localStorage to the specified file")
+                .help("Output cookies and localStorage to the specified file.")
                 .action(ArgAction::Set),
         )
         .arg(
             Arg::new("wait")
                 .long("wait")
                 .short('w')
-                .help("Hang browser after finishing tasks")
+                .help("Hang browser after executing tasks.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("confirm")
                 .long("confirm")
                 .short('y')
-                .help("Confirm before executing tasks")
+                .help("Confirm before executing tasks.")
                 .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("user-agent")
                 .long("user-agent")
                 .short('a')
-                .help("Specify a custom User-Agent (requires --new)")
+                .help("Specify a custom User-Agent (requires '--new).")
                 .requires("new")
                 .action(ArgAction::Set),
+        )
+        .arg(
+            Arg::new("incognito")
+                .long("incognito")
+                .short('i')
+                .help("Enable incognito mode.")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("stealth")
+                .long("stealth")
+                .short('s')
+                .help("Enable stealth mode (see more with '--help').")
+                .long_help(vec!["Stealth mode may not work as intended and only lead to an easier detection of an automated browser.", "Stealth mode tries to bypass (generalize) the following JS objects: webdriver, permissions, plugins, webgl vendor."].join("\n"))
+                .action(ArgAction::SetTrue),
         )
         .arg(
             Arg::new("verbose")
                 .long("verbose")
                 .short('v')
-                .help("Enable verbose output")
+                .help("Enable verbose output.")
                 .action(ArgAction::SetTrue),
         )
         .group(
@@ -111,7 +127,7 @@ fn main() {
         )
         .get_matches();
 
-    let is_verbose: bool = *matches.get_one::<bool>("verbose").unwrap();
+    let is_verbose = *matches.get_one::<bool>("verbose").unwrap();
 
     macro_rules! debug {
         ($($arg:tt)*) => {
@@ -125,41 +141,54 @@ fn main() {
         ($($arg:tt)*) => {
             println!("[{}] {}", crate_name!(), format!($($arg)*))
         };
-        }
+    }
 
-    let is_clean: bool = *matches.get_one::<bool>("clean").unwrap();
-    let is_wait: bool = *matches.get_one::<bool>("wait").unwrap();
-    let is_confirm: bool = *matches.get_one::<bool>("confirm").unwrap();
+    let is_clean = *matches.get_one::<bool>("clean").unwrap();
+    let is_wait = *matches.get_one::<bool>("wait").unwrap();
+    let is_confirm = *matches.get_one::<bool>("confirm").unwrap();
+    let is_incognito = *matches.get_one::<bool>("incognito").unwrap();
+    let is_stealth = *matches.get_one::<bool>("stealth").unwrap();
 
-    let browser: Browser;
+    let browser;
 
-    let mut connect_url: &str = "";
-    let is_connect: bool = matches.contains_id("connect");
+    let mut connect_url = "";
+    let is_connect = matches.contains_id("connect");
 
-    let mut output_file: &str = "";
-    let is_output: bool = matches.contains_id("output");
+    let mut output_file = "";
+    let is_output = matches.contains_id("output");
 
-    let mut payload_file: &str = "";
-    let is_payload: bool = matches.contains_id("payload");
+    let mut payload_file = "";
+    let is_payload = matches.contains_id("payload");
 
     if is_connect {
         connect_url = matches.get_one::<String>("connect").unwrap();
         print!("Connecting to existing browser instance at {}", connect_url);
         browser = Browser::connect(connect_url.to_owned()).unwrap();
     } else {
-        let headful: bool = *matches.get_one::<bool>("headful").unwrap();
-        let user_agent: &str = matches
-            .get_one::<String>("user-agent")
-            .map_or(USER_AGENT, |v| v);
+        let headful = *matches.get_one::<bool>("headful").unwrap();
+        let user_agent_arg = format!(
+            "--user-agent={}",
+            matches
+                .get_one::<String>("user-agent")
+                .map_or(USER_AGENT, |v| v)
+        );
 
         print!(
             "Launching new {} browser instance",
             if headful { "headless" } else { "headful" }
         );
 
+        let mut args = vec![OsStr::new(&user_agent_arg)];
+
+        if is_incognito {
+            debug!("Launching instance with incognito flag");
+            args.push(&OsStr::new("--incognito"))
+        }
+
         browser = Browser::new(headless_chrome::LaunchOptions {
             headless: headful,
-            args: vec![OsStr::new(&format!("--user-agent={}", user_agent))],
+            args: args,
+
             ..Default::default()
         })
         .unwrap();
@@ -176,14 +205,29 @@ fn main() {
     }
 
     let version = browser.get_version().unwrap();
-    debug!(
+    debug!( 
         "Browser information:\n\t- User-Agent: {}\n\t- Product information: {}\n\t- JavaScript Version: {}",
         version.user_agent, version.product, version.js_version
     );
+    
+    let tab;
 
-    let tab = browser.new_tab().unwrap();
+    if is_connect {
+        if is_incognito {
+            let context = browser.new_context().unwrap();
+            tab = context.new_tab().unwrap();
+        } else {
+            tab = browser.new_tab().unwrap();
+        }
+    } else {
+        tab = browser.new_tab().unwrap();
+    }
 
-    tab.enable_stealth_mode().unwrap();
+    if is_stealth {
+        debug!("Using stealth mode. (may make detection easier, considering turning it off)");
+        tab.enable_stealth_mode().unwrap();
+    }
+
     if matches.contains_id("url") {
         tab.navigate_to(matches.get_one::<String>("url").unwrap())
             .unwrap();
@@ -199,14 +243,14 @@ fn main() {
         if is_clean {
             debug!("Deleting cookies...");
 
-            let delete_cookies: Vec<Cookie>;
+            let delete_cookies;
             match tab.get_cookies() {
                 Ok(cookie_vec) => {
                     delete_cookies = cookie_vec;
                 }
 
                 Err(e) => {
-                    print!("Panicked while fetching cookies: {}", e);
+                    print!("Error occurred while fetching cookies: {}", e);
                     std::process::exit(1);
                 }
             }
@@ -231,19 +275,19 @@ fn main() {
             tab.wait_until_navigated().unwrap();
 
             // wait_until_navigated is not enough when reloading; allow page to load before fetching cookies
-            std::thread::sleep(std::time::Duration::from_millis(500));
+            std::thread::sleep(std::time::Duration::from_millis(600));
 
             debug!("Cookies deleted successfully");
         }
 
-        let cookies: Vec<Cookie>;
+        let cookies;
         match tab.get_cookies() {
             Ok(cookie_vec) => {
                 cookies = cookie_vec;
             }
 
             Err(e) => {
-                print!("Panicked while fetching cookies: {}", e);
+                print!("Error occurred while fetching cookies: {}", e);
                 std::process::exit(1);
             }
         }
@@ -253,7 +297,7 @@ fn main() {
         match std::fs::write(output_file, serde_json::to_string(&cookies).unwrap()) {
             Ok(_) => print!("Cookies successfully saved to '{}'", output_file),
             Err(e) => {
-                print!("Panicked while writing cookies: {}", e);
+                print!("Error occurred while writing cookies: {}", e);
                 std::process::exit(1);
             }
         }
@@ -263,7 +307,7 @@ fn main() {
         debug!("Reading payload from '{}'...", payload_file);
 
         let read = std::fs::read_to_string(payload_file);
-        let mut payload: String = "".to_owned();
+        let mut payload = "".to_owned();
 
         match read {
             Ok(bytes_read) => {
@@ -272,7 +316,7 @@ fn main() {
             }
 
             Err(e) => {
-                print!("Panicked reading payload file: {}", e);
+                print!("Error occurred reading payload file: {}", e);
                 std::process::exit(1);
             }
         }
@@ -281,7 +325,7 @@ fn main() {
         match tab.evaluate(&payload, false) {
             Ok(_) => print!("Payload executed successfully"),
             Err(e) => {
-                print!("Panicked while executing payload: {}", e);
+                print!("Error occurred while executing payload: {}", e);
                 std::process::exit(1);
             }
         };
